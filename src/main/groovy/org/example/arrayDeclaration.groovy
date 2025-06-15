034f04ffee531457ce0c14072bd3db4818eb0691
Bhavesh <bhaveshverma101@gmail.com>
2025-06-15 06:05:08 +0530

    GROOVY

---
+++ src/main/groovy/org/example/arrayDeclaration.groovy
@@ -0,0 +1,12 @@
+package org.example
+
+static void main(String[] args) {
+    println "Hello world!"
+
+    int[] array = new int[] {1, 3 ,3, 4} //method 1
+    println(array)
+
+    int[] array2 = [ 1 , 2 , 3 ] //method 2
+    println(array2)
+
+}
